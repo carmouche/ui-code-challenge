95f268aa4bfa867c9d878744110d36ab189bee19
Selena Carmouche <selena.carmouche@LAX-DPC-01086.local>
2016-05-26 14:01:19 -0700

    Mapped band names to musician

--- app/routes/musicians.js
+++ app/routes/musicians.js
@@ -3,7 +3,16 @@ import camelcaseObject from 'npm:camelcase-object';
 import MusiciansModel from '../models/musicians';
 
 export default Ember.Route.extend({
+  mapBandsToMusicians() {
+  	MusiciansModel.Musicians.forEach((musician) => {
+  		musician.bandName = MusiciansModel.Bands.filter((band) => {
+  			return band.id === musician.band;
+  		})[0].name;
+  	});
+
+  	return MusiciansModel;
+  },
   model() {
-    return camelcaseObject(MusiciansModel);
+    return camelcaseObject(this.mapBandsToMusicians());
   }
 });
